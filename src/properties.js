6fd742e95f34cd3f4ca842f6dea1e033cb6591a7
Aditya Khandare <khandareaditya97@gmail.com>
2022-10-31 21:01:56 -0500

    added Search functionality, card open for additional details

---
+++ src/properties.js
@@ -0,0 +1,69 @@
+export default[
+    {
+        title:"Yellow & Blue Elegant Container",
+        city:"Waco,Texas",
+        category:"Tiny Home",
+        description:"Try out container home living in this unique tiny home! Sleeps 2 people in the custom Murphy® bed with a super-comfy Tuft & Needle™ queen size mattress. Kitchen and dining area features induction cooktop, fridge and custom knotty alder tabletop. Hang out on the spacious rooftop deck that lights up beautifully at night with the color-adjustable LED rail lights. Full-size, custom tiled shower and spacious bathroom completes this fascinating container home.",
+        nightly_fee:"$90",
+        cleaning_fee:"$35",
+        service_fee:"$68",
+        amenities:"hair dryer,hot water,extra pillowa,hangers,iron,TV,AC,smako alarm",
+        bedrooms:"1",
+        map_address:"",
+        img:"1.png"
+    },
+    {
+        title:"Little Red Treehouse",
+        city:"Lyons,Colorado",
+        category:"Treehouse",
+        description:"The Little Red Tree house accommodates two guests only. It has huge views, private shower, with separate powder room sink and toilet. It has an efficiency kitchen, with small sink and counter top,as well as a fridge. The tree house is equipped with heat/air and electricity. Located on the way to Rocky Mountain NP, directly across from Rocky Grass There is a full size pull down Murphy bed which sleeps two , the fairy loft sleeps one.Total occupancy two people maximum !",
+        nightly_fee:"$250",
+        cleaning_fee:"$40",
+        service_fee:"$182",
+        amenities:"Kitchen,WiFi,Free Parking,Patio,AC,Backyard,Microwave",
+        bedrooms:"1",
+        map_address:"",
+        img:"2.png"
+    },
+    {
+        title:"Three Pines Petersburg",
+        city:"Patersburg,Illinois",
+        category:"Mansion",
+        description:"Perched high atop one of Petersburg's many hills, this beautiful Italianate Mansion enjoys a birds-eye-view of historic Petersburg.  The home is nestled among beautiful trees and landscaping in a quiet, peaceful setting. We are located a half mile from the town square.",
+        nightly_fee:"$680",
+        cleaning_fee:"$350",
+        service_fee:"$529",
+        amenities:"Kitchen,WiFi,Free Parking,TV,Dryer,Pets allowed,Patio",
+        bedrooms:"10",
+        map_address:"",
+        img:"3.png"
+    },
+    {
+        title:"Garden View Suite",
+        city:"Overton,Texas",
+        category:"Countryside",
+        description:"King Cottage is a modern country cottage constructed in 2017. The atmosphere, created by design, has the feel of a timeless home that has stood the test of time. Centrally located in the heart of East Texas, the cottage sits atop a hill, on acreage, surrounded by 80 foot majestic pine trees. Flanked by a common living area, the Garden View Suite is a spacious 572 square foot suite with one king and one twin bed. The suite also includes a private bath.",
+        nightly_fee:"$90",
+        cleaning_fee:"$25",
+        service_fee:"$67",
+        amenities:"WiFi,TV,Dryer,Backyard,Refirgerator,Washer,Hair Dryer",
+        bedrooms:"1",
+        map_address:"",
+        img:"4.png"
+    },
+    {
+        title:"Little Joshua Tree",
+        city:"Joshua Tree,California",
+        category:"Camper",
+        description:"Little Joshua Tree is a secluded, 5-acre desert oasis featuring a remodeled one bed, one bath homestead cabin with a cozy living room and sleeping space with queen bed. There are two outdoor dining areas and it's an amazing location for stargazing. The property is surrounded by scenic mountain and desert views.",
+        nightly_fee:"$190",
+        cleaning_fee:"$135",
+        service_fee:"$154",
+        amenities:"Desert View,Kitchen,TV,Dryer,Mountain View,WiFi,AC",
+        bedrooms:"1",
+        map_address:"",
+        img:"5.png"
+    }
+
+]
+
